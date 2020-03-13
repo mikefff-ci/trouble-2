4480dd5b6b066fb97341f86107b4785972ee06f6
Mike Lay <mike@mkly.io>
2020-03-13 15:10:05 -0700

    DD

--- trap.py
+++ trap.py
@@ -1,32 +1,13 @@
 #!/usr/bin/env python3
-import os
-import sys
 import time
 import signal
 
-SIGNALS_TO_NAMES_DICT = dict((getattr(signal, n), n) \
-    for n in dir(signal) if n.startswith('SIG') and '_' not in n )
-
-
 def receive_signal(signum, stack):
-    if signum in [1,2,3,15]:
-        print('Caught signal %s (%s), exiting.' % (SIGNALS_TO_NAMES_DICT[signum], str(signum)))
-        for i in range(10000):
-            print('start')
-            time.sleep(1)
-            print ('still time')
-    else:
-        print('Caught signal %s (%s), ignoring.' % (SIGNALS_TO_NAMES_DICT[signum], str(signum)))
+    print('Caught SIGINT');
 
 def main():
-    uncatchable = ['SIG_DFL','SIGSTOP','SIGKILL']
-    for i in range(signal.NSIG):
-        print(i)
-        try:
-            signal.signal(i,receive_signal)
-        except:
-            print(i)
-    print('My PID: %s' % os.getpid())
+    signal.signal(signal.SIGINT,receive_signal)
+
     while True:
         time.sleep(1)
 main()
