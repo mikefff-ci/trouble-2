ad93b455a14ba6edb88819b7e9da7d24d7702663
Mike Lay <mike@mkly.io>
2020-05-01 09:55:25 -0700

    manve time

--- src/test/java/com.mkly.examples/TestGoogleSearch.java
+++ src/test/java/com.mkly.examples/TestGoogleSearch.java
@@ -17,23 +17,44 @@ public class TestGoogleSearch {
   @Test
   public void google_Search()
   {
-      //System.setProperty("webdriver.chrome.driver", "/Users/mikelay/Downloads/chromedriver");
+      System.setProperty("webdriver.chrome.driver", "/Users/mikelay/Downloads/chromedriver");
       WebDriver driver;
       driver = new ChromeDriver();
       driver.get("http://www.google.com");
-      driver.manage().window().maximize();
+      //driver.manage().window().maximize();
+      System.out.println("Running here");
+      System.out.println("Running here");
+      System.out.println("Running here");
+      System.out.println("Running here");
+      System.out.println("Running here");
+      System.out.println("Running here");
+      System.out.println("Running here");
+      System.out.println("Running here");
+      System.out.println("Running here");
+      System.out.println("Running here");
+      System.out.println("Running here");
+      System.out.println("Running here");
+      System.out.println("Running here");
+      System.out.println("Running here");
+      System.out.println("Running here");
+      System.out.println("Running here");
+      System.out.println("Running here");
 
-      WebElement element = driver.findElement(By.name("q"));
-      element.sendKeys("Cheese!\n");
-      element.submit();
+      //WebElement element = driver.findElement(By.name("q"));
+      //element.sendKeys("Cheese!\n");
+      //element.submit();
 
       //Wait until the google page shows the result
-      WebElement myDynamicElement = (new WebDriverWait(driver, 10)).until(ExpectedConditions.presenceOfElementLocated(By.id("resultStats")));
+      //WebElement myDynamicElement = (new WebDriverWait(driver, 10)).until(ExpectedConditions.presenceOfElementLocated(By.id("resultStats")));
 
-      List<WebElement> findElements = driver.findElements(By.xpath("//*[@id='rso']//h3/a"));
+      //List<WebElement> findElements = driver.findElements(By.xpath("//*[@id='rso']//h3/a"));
 
       //Get the url of third link and navigate to it
-      String third_link = findElements.get(2).getAttribute("href");
-      driver.navigate().to(third_link);
+      //String third_link = findElements.get(2).getAttribute("href");
+      //driver.navigate().to(third_link);
+
+      System.out.println(driver.getTitle());
+
+      driver.quit();
   }
 }
